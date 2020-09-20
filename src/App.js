3a0f631afb1c23137a0ef39c82546ddae5ff65c5
jenniekibiri <josphatje@gmail.com>
2020-09-20 05:34:17 -0400

    create sidebar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './css/sb-admin-2.css'
+import './vendor/fontawesome-free/css/all.min.css'
+import Sidebar from './components/Sidebar'
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Sidebar/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
