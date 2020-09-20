e1035f6535f4d85cb6b24c97d6ba94bbccc6ef1a
jenniekibiri <josphatje@gmail.com>
2020-09-20 06:05:33 -0400

    add top nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import './css/sb-admin-2.css'
 import './vendor/fontawesome-free/css/all.min.css'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Sidebar/>
+      <div id="page-top">  
+        <div id="wrapper">
+        <Sidebar
+      
+      />
+      <Navbar/>
+      </div>
       </div>
     )
   }
